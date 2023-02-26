eaf85de16f2cea288af54d0ab503d3196fc0e087
Noronha1612 <inc.691@gmail.com>
2023-02-26 18:04:45 -0300

    Create app header in app to be shown in every page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import logoImg from '@/assets/logo.svg'
+import * as S from '@/styles/pages/app'
+import Image from 'next/image'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <S.Container>
+      <S.Header>
+        <Image src={logoImg} alt="Logo Image" />
+      </S.Header>
+
+      <Component {...pageProps} />
+    </S.Container>
   )
 }

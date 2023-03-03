6d04c1502261a5590297b15699ba493a39398013
Noronha1612 <inc.691@gmail.com>
2023-03-02 21:07:58 -0300

    update app_url in production environment

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -18,8 +18,13 @@ export async function POST(request: Request) {
 
   const totalItemsQuantity = items.reduce((acc, item) => acc + item.quantity, 0)
 
-  const success_url = `${process.env.APP_URL}/success?session_id={CHECKOUT_SESSION_ID}&quantity=${totalItemsQuantity}`
-  const cancel_url = `${process.env.APP_URL}`
+  const appUrl =
+    process.env.NODE_ENV === 'production'
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : process.env.APP_URL
+
+  const success_url = `${appUrl}/success?session_id={CHECKOUT_SESSION_ID}&quantity=${totalItemsQuantity}`
+  const cancel_url = `${appUrl}`
 
   const checkoutSession = await stripe.checkout.sessions.create({
     mode: 'payment',
